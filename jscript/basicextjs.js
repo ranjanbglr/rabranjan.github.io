233f2125d0105329e3d84e53f393aa25a9ee5c26
rabranjan <rab.ranjan@gmail.com>
2015-05-15 11:27:32 +0530

    Extjs basics learning

---
+++ jscript/basicextjs.js
@@ -0,0 +1,24 @@
+Ext.onReady(function(){
+	Ext.Msg.alert("Config status","Hi, Ext Js is configured successfully :)");
+	
+	var grdStore = Ext.create('Ext.data.Store',{
+		fields: ['name','address'],
+		//data: [{'name':'Ravi','address':'Jharkhand'},{'name':'Ashish','address':'Jharkhand'},{'name':'Nilesh','address':'Jharkhand'}]
+				autoLoad: true,
+		proxy: {
+			type: 'ajax',
+			url: '../devr/ext-js/simple-grid.txt',
+			reader: {
+				type: 'json',
+				root: ''
+			}
+		}
+	});
+	Ext.create('Ext.grid.Panel',{
+		title: 'sample grid',
+		columns: [{dataIndex:'name',header:'Name'},{dataIndex:'address',header:'Address'}],
+		store: grdStore,
+		width: 300,
+		renderTo: Ext.getBody(),
+	})
+});
