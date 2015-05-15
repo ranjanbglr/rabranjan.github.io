c69afe5de3496279cb24ea57f34b8f04dc050086
rabranjan <rab.ranjan@gmail.com>
2015-05-15 12:05:07 +0530

    min

--- jscript/basicextjs.js
+++ jscript/basicextjs.js
@@ -1,5 +1,23 @@
 Ext.onReady(function(){
+	Ext.QuickTips.init();
 	Ext.Msg.alert("Config status","Hi, Ext Js is configured successfully :)");
+	
+	Ext.create('Ext.Button', {
+		text: 'Tooltip button',
+		renderTo: Ext.getBody(),
+		// handler: function(){
+			// Ext.Msg.alert('You clicked Me')
+		// },
+		tooltip: {
+			text: "Hey! you moved mouse cursor into my area :)",
+			width: 100
+		},
+		listeners : {
+			'click':function(){
+				Ext.Msg.alert('You clicked Me');
+			}
+		}
+	});
 	
 	var grdStore = Ext.create('Ext.data.Store',{
 		fields: ['name','address'],
